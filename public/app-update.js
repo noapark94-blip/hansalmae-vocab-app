f127202db13aef1f8a563428f09c1ee81a907e72
noapark94-blip <noapark94@gmail.com>
2026-09-25 01:36:32 +0900

    Keep unowned emblem artwork dimmed in details too

--- public/app-update.js
+++ public/app-update.js
@@ -5,7 +5,7 @@
   window.__HANSALMAE_APP_UPDATE_COORDINATOR__ = true;
 
   var BUILD_VERSION = String(
-    window.HANSALMAE_BUILD_VERSION || '20260924-40'
+    window.HANSALMAE_BUILD_VERSION || '20260924-41'
   );
   var RELOAD_GUARD_KEY = 'hsmAppUpdateReloadGuard';
   var UPDATED_TO_KEY = 'hsmAppUpdatedTo';
