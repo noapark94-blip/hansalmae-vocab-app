e641e28f07481ab5e990c114d12f819a5b856e06
noapark94-blip <noapark94@gmail.com>
2026-09-25 00:55:16 +0900

    Enlarge level progress and emblem artwork with balanced compact layout

--- public/app-update.js
+++ public/app-update.js
@@ -5,7 +5,7 @@
   window.__HANSALMAE_APP_UPDATE_COORDINATOR__ = true;
 
   var BUILD_VERSION = String(
-    window.HANSALMAE_BUILD_VERSION || '20260924-30'
+    window.HANSALMAE_BUILD_VERSION || '20260924-31'
   );
   var RELOAD_GUARD_KEY = 'hsmAppUpdateReloadGuard';
   var UPDATED_TO_KEY = 'hsmAppUpdatedTo';
