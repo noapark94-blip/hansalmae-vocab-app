3b13d542367a5d79ed227fd3e2ccfe20d7ea4738
noapark94-blip <noapark94@gmail.com>
2026-09-25 02:58:59 +0900

    Polish school vocabulary reading, selection and exam settings dialog

--- public/app-update.js
+++ public/app-update.js
@@ -5,7 +5,7 @@
   window.__HANSALMAE_APP_UPDATE_COORDINATOR__ = true;
 
   var BUILD_VERSION = String(
-    window.HANSALMAE_BUILD_VERSION || '20260925-53'
+    window.HANSALMAE_BUILD_VERSION || '20260925-54'
   );
   var RELOAD_GUARD_KEY = 'hsmAppUpdateReloadGuard';
   var UPDATED_TO_KEY = 'hsmAppUpdatedTo';
