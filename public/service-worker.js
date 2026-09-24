26b9c24318ccae5b9154450ed9b9500c46580cce
noapark94-blip <noapark94@gmail.com>
2026-09-25 07:39:40 +0900

    Refine chunk study tiles, morphology fields and action hierarchy

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260925-61';
+const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260925-62';
 const CACHE_NAME = 'hansalmae-supabase-build-' + BUILD_VERSION;
 const versioned = function (path) {
   return path + '?v=' + encodeURIComponent(BUILD_VERSION);
