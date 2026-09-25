707c06ef48fa05ed2980c5704b978d272e796c34
noapark94-blip <noapark94@gmail.com>
2026-09-25 20:04:25 +0900

    Style friend battle entry as an arcade card with original chick emblems

--- public/app-update.js
+++ public/app-update.js
@@ -5,7 +5,7 @@
   window.__HANSALMAE_APP_UPDATE_COORDINATOR__ = true;
 
   var BUILD_VERSION = String(
-    window.HANSALMAE_BUILD_VERSION || '20260925-72'
+    window.HANSALMAE_BUILD_VERSION || '20260925-73'
   );
   var RELOAD_GUARD_KEY = 'hsmAppUpdateReloadGuard';
   var UPDATED_TO_KEY = 'hsmAppUpdatedTo';
