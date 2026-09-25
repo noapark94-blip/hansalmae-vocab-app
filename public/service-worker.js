3697c668e960907400a24d0792ffe21848bc1135
noapark94-blip <noapark94@gmail.com>
2026-09-25 20:19:43 +0900

    Unify battle poster with cream title and gold entry button

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260925-74';
+const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260925-75';
 const CACHE_NAME = 'hansalmae-supabase-build-' + BUILD_VERSION;
 const versioned = function (path) {
   return path + '?v=' + encodeURIComponent(BUILD_VERSION);
