bad3d1991ba62a528a425c0165d46473b0ca24a3
noapark94-blip <noapark94@gmail.com>
2026-09-26 23:41:44 +0900

    Refine battle and result screens; remove character collision intro

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260926-172';
+const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260926-173';
 const CACHE_NAME = 'hansalmae-supabase-build-' + BUILD_VERSION;
 const versioned = function (path) {
   return path + '?v=' + encodeURIComponent(BUILD_VERSION);
