9d3f4c42f230ce0b3bdabf387eb0f40ccc923a35
noapark94-blip <noapark94@gmail.com>
2026-09-26 10:21:01 +0900

    Use VS faceoff artwork in battle lobby banner

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260926-99';
+const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260926-100';
 const CACHE_NAME = 'hansalmae-supabase-build-' + BUILD_VERSION;
 const versioned = function (path) {
   return path + '?v=' + encodeURIComponent(BUILD_VERSION);
