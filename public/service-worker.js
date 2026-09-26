5c8bb8999245a0fb6aa47e325730b20dfefc517c
noapark94-blip <noapark94@gmail.com>
2026-09-26 16:39:15 +0900

    Remove duplicate top inset in iPhone standalone student screens

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260926-137';
+const BUILD_VERSION = new URL(self.location.href).searchParams.get('v') || '20260926-138';
 const CACHE_NAME = 'hansalmae-supabase-build-' + BUILD_VERSION;
 const versioned = function (path) {
   return path + '?v=' + encodeURIComponent(BUILD_VERSION);
